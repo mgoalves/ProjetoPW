e580940ed3225848f3a0eb3eea2e1421915e8ce7
JonesQuito <Jonesdhy@hotmail.com>
2017-06-28 00:02:29 -0300

    Alateração no ServletLogin para usar o método de altenticar usuário que retorna um objeto do tipo usuário

--- src/main/java/br/com/labpw/servletController/ServletLogin.java
+++ src/main/java/br/com/labpw/servletController/ServletLogin.java
@@ -18,49 +18,42 @@ import br.com.labpw.model.UsuarioDao;
 @SuppressWarnings("serial")
 @WebServlet("/servletlogin")
 public class ServletLogin extends HttpServlet {
-	
+
 	protected void service(HttpServletRequest request, HttpServletResponse response)
-							throws IOException, ServletException{
-		
-		//captura os parâmetros passados na requisição
+			throws IOException, ServletException {
+
+		// captura os parâmetros passados na requisição
 		String usuarioStr = request.getParameter("usuario");
 		String senhaStr = request.getParameter("senha");
-		
-		if((usuarioStr == null || senhaStr == null) || (usuarioStr.isEmpty() || senhaStr.isEmpty())){
+
+		if ((usuarioStr == null || senhaStr == null) || (usuarioStr.isEmpty() || senhaStr.isEmpty())) {
 			request.getRequestDispatcher("/index.html").forward(request, response);
-			request.setAttribute("menssagem", "Informar login e senha!!");
-			//response.sendRedirect("index.jsp");
-		}else{
-			// Recupera uma conexão com o banco			
+		} else {
+			// Recupera uma conexão com o banco
 			Connection connection = new ConnectionFactory().getConnection();
-			
-			//Instancia um objeto Usuario e seta os valores recebidos na requisiação
+
+			// Instancia um objeto Usuario e seta os valores recebidos na
+			// requisiação
 			Usuario usuario = new Usuario();
 			usuario.setNome(usuarioStr);
 			usuario.setSenha(senhaStr);
-			
-			//Instancia um UsuarioDao e executa o método de autenticar
+
+			// Instancia um UsuarioDao e executa o método de autenticar
 			UsuarioDao dao = new UsuarioDao(connection);
-			if(dao.autenticar(usuario)){
+			Usuario usuarioAutenticado = dao.autenticar(usuario);
+			if (usuarioAutenticado != null) {
 				HttpSession sessao = request.getSession();
-				sessao.setAttribute("usuario", usuario);
-				sessao.setAttribute("logado", true);
-				sessao.setMaxInactiveInterval(1*60);
+				sessao.setAttribute("usuario", usuarioAutenticado);
+				sessao.setMaxInactiveInterval(2 * 60);
 				request.getRequestDispatcher("/home.jsp").forward(request, response);
-				
-			}else{
-				///////////////
-				HttpSession sessao = request.getSession();
-				sessao.setAttribute("logado", false);
-				sessao.setMaxInactiveInterval(300);
-				/////////////
+
+			} else {
 				request.setAttribute("menssagem", "Usuário e/ou senha inválidos!!!");
-				request.getRequestDispatcher("/erroPage.jsp").forward(request, response);;
+				request.getRequestDispatcher("/erroPage.jsp").forward(request, response);
+				;
 			}
 		}
-		
-		
-			
+
 	}
 
 }
