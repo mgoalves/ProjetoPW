2e047825d9e8ac932bfb5e6ccfed9d70b63c3695
JonesQuito <Jonesdhy@hotmail.com>
2017-06-28 00:44:02 -0300

    Ajustes nos atributos e respectivos métodos de acesso nas classes Aluno e Endereco

--- src/main/java/br/com/labpw/model/Aluno.java
+++ src/main/java/br/com/labpw/model/Aluno.java
@@ -1,19 +1,66 @@
 package br.com.labpw.model;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Hitallo, Jones, Marcello e Reinaldo.
  */
 
-public class Aluno extends Endereco {
-    
-     private String rg;
-     private String cpf;
-     private String nome;
-     private String nomeMae;
-     private int matricula;
-     private String dataNascimento;
-     
-     Endereco endeco;
-     
+public class Aluno {
+
+	// Atributos de um objeto do tipo Aluno
+	private String matricula;
+	private String nome;
+	private Calendar dataNascimento;
+	private String rg;
+	private String cpf;
+	private String nomeMae;
+	private Endereco endereco;
+	
+	
+	// Métodos acessores dos atributos de um objeto Aluno
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
 }
--- src/main/java/br/com/labpw/model/Endereco.java
+++ src/main/java/br/com/labpw/model/Endereco.java
@@ -2,9 +2,44 @@ package br.com.labpw.model;
 
 public abstract class Endereco {
     
-    String cidade;
-    String bairro;
-    int numero;
-    String logradouro;
-    
+	//Atributos de um objeto Endereco
+	private String cidade;
+	private String bairro;
+	private String logradouro;
+	private String cep;
+	private int numero;
+		
+	
+	//Métodos acessores para os atributos de um objeto Endereco
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
 }
