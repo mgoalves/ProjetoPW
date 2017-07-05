6bcbf019029a583af6d353ddadea355ffefaa00d
Marcello <marcello_doalves@hotmail.com>
2017-07-04 22:06:55 -0300

    Execução da classe disciplina

--- src/main/java/br/com/labpw/model/Disciplina.java
+++ src/main/java/br/com/labpw/model/Disciplina.java
@@ -1,10 +1,45 @@
 package br.com.labpw.model;
 
 public class Disciplina {
-	
-	String nome;
-	String duracao;
-	int cargaHoraria;
-	
 
+	// Atributos de um objeto do tipo Disciplina
+	private int codigo;
+	private String nome;
+	private String descricao;
+	private int cargaHoraria;
+
+	
+	
+	// Getters and Setters
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
 }
