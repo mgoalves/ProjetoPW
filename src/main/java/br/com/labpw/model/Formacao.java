37924da120acda8318c22cd171f815ab1b356047
Marcello <marcello_doalves@hotmail.com>
2017-07-06 00:05:39 -0300

    Alterações nos modais professor e formação

--- src/main/java/br/com/labpw/model/Formacao.java
+++ src/main/java/br/com/labpw/model/Formacao.java
@@ -1,10 +1,36 @@
 package br.com.labpw.model;
 
 public class Formacao {
-	
-	String mestrado;
-	String graduacao;
-	String posGraduacao;
-	
+
+	// Atributos da Classe Formação
+	private String mestrado;
+	private String graduacao;
+	private String posGraduacao;
+
+	// Getters and Setters
+	// Metodos acessores
+	public String getMestrado() {
+		return mestrado;
+	}
+
+	public void setMestrado(String mestrado) {
+		this.mestrado = mestrado;
+	}
+
+	public String getGraduacao() {
+		return graduacao;
+	}
+
+	public void setGraduacao(String graduacao) {
+		this.graduacao = graduacao;
+	}
+
+	public String getPosGraduacao() {
+		return posGraduacao;
+	}
+
+	public void setPosGraduacao(String posGraduacao) {
+		this.posGraduacao = posGraduacao;
+	}
 
 }
--- src/main/java/br/com/labpw/model/Professor.java
+++ src/main/java/br/com/labpw/model/Professor.java
@@ -1,16 +1,61 @@
 package br.com.labpw.model;
 
 public class Professor {
+
+	// Atributos da classe professor
+	private String rg;
+	private String cpf;
+	private String nome;
+	private int matricula;
+	private String dataNascimento;
+
+	// Atributos importados
+	private Endereco end;
+	private Formacao form;
 	
-	String rg;
-	String cpf;
-	String nome;
-	int matricula;
-	String dataNascimento;
 	
-	Endereco end;
-	
-	Formacao form;
-	
+	//Getters and Setters
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Endereco getEnd() {
+		return end;
+	}
+	public void setEnd(Endereco end) {
+		this.end = end;
+	}
+	public Formacao getForm() {
+		return form;
+	}
+	public void setForm(Formacao form) {
+		this.form = form;
+	}
 
 }
